8a3933f6ade51a5d0c8ea5ea87d6c8608a5833ac
Elizabeth Guerra <ecguerra@outlook.com>
2020-11-10 01:31:24 -0500

    split shelter info into usable data pieces

--- controllers/shelters.js
+++ controllers/shelters.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const db = require('../models')
 const request = require('request')
 const cheerio = require('cheerio')
-const URL = 'https://www.homelessshelterdirectory.org/cgi-bin/id/city.cgi?city=Boston&state=MA'
+let URL = 'https://www.homelessshelterdirectory.org/cgi-bin/id/city.cgi?city=Boston&state=MA'
 
 // GET /shelters/search
 router.get('/search', (req,res) => {
@@ -37,9 +37,25 @@ router.get('/:id',(req,res) => {
         let $ = cheerio.load(body)
         let results = $('.entry_content')
         let resultDetails = results.map((index,element) => {
+            let fullDetails = $(element).find('p').text()
+            let addr = fullDetails.substring(fullDetails.indexOf(':'),0)
+            let city = $(element).find('h3').text()
+
+            city = city.substring(city.lastIndexOf('-')+2,city.indexOf(','))
+            
+            let street = addr.substring(0,addr.length-addr.indexOf(city)+4)
+            let state = addr.substring(addr.indexOf(',')+2,addr.indexOf(',')+4)
+            let zip = addr.substring(addr.indexOf(',')+5,addr.indexOf(',')+10)
+            let phone = fullDetails.substring(fullDetails.indexOf('Phone:')+7,fullDetails.indexOf('Phone:')+23)          
             return {
                 name: $(element).find('h3').text(),
-                address: $(element).find('p').text()
+                address: $(element).find('p').text(),
+                addr: addr,
+                street: street,
+                city: city,
+                state: state,
+                zip: zip,
+                phone: phone
             }
         })
         res.render('shelters/show.ejs',{shelter: resultDetails.get()})
