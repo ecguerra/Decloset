6b514c1862bdd1442061f5ae360343eec8e5d588
Elizabeth Guerra <ecguerra@outlook.com>
2020-11-16 22:49:28 -0500

    protected clothing EDIT routes

--- controllers/clothing.js
+++ controllers/clothing.js
@@ -84,48 +84,96 @@ router.get('/:id', isLoggedIn, (req,res) => {
 
 // GET /clothing/edit/:id
 router.get('/edit/:id', isLoggedIn, (req,res) => {
-    db.clothing.findOne({
-        where: {id: req.params.id},
-        include: [db.category, db.shelter]
-    })
-    .then(clothing => {
-        res.render('clothing/edit', {clothing: clothing})
-    })
-    .catch(err => {
-        console.log(err)
+    db.user.findByPk(req.user.id)
+    .then(user => {
+        user.getClothings({where: {id: req.params.id}, include: [db.category, db.shelter]})
+        .then(clothing => {
+            res.render('clothing/edit', {clothing: clothing})
+        })
+        .catch(err => {
+            console.log(err)
+        })
     })
 })
 
 // PUT /clothing/edit/:id
-router.put('/:id', async(req,res) => {
-    try{
-        await db.clothing.update({
+router.put('/:id', isLoggedIn, (req,res) => {
+    db.user.findByPk(req.user.id)
+    .then(user => {
+        user.getClothings({where: {id: req.params.id}, include: [db.category, db.shelter]})
+        .then(clothing => {
+            db.clothing.update({
             style: req.body.style,
             status: req.body.status,
             brand: req.body.brand,
             material: req.body.material,
             color: req.body.color,
             condition: req.body.condition
-        },
+            },
         {
             where: {id: req.body.id},
             include: [db.category]
         })
-        const foundClothing = await db.clothing.findOne({
+        db.clothing.findOne({
             where: {id: req.body.id},
             include: [db.category]
         })
-        const [foundOrCreated, isCreated] = await db.category.findOrCreate({
-            where: {name: req.body.category}
+        .then(foundClothing => {
+            db.category.findOrCreate({
+                where: {name: req.body.category}
+            })
+            .then(([newCategory, created]) => {
+                foundClothing.setCategory(newCategory)
+                res.redirect(`/clothing/${req.body.id}`)
+            })
+            .catch(err => {
+                console.log(err)
+            })
         })
-        await foundClothing.setCategory(foundOrCreated)
-        res.redirect(`/clothing/${req.body.id}`)
-    }
-    catch(err){
+        .catch(err => {
+            console.log(err)
+        })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    })
+    .catch(err => {
         console.log(err)
-        res.redirect(`/clothing/${req.body.id}`)
-    }
+    })
 })
+
+
+// ASYNC WAY
+// router.put('/:id', isLoggedIn, async(req,res) => {
+//     try{
+//         await db.clothing.update({
+//             style: req.body.style,
+//             status: req.body.status,
+//             brand: req.body.brand,
+//             material: req.body.material,
+//             color: req.body.color,
+//             condition: req.body.condition
+//         },
+//         {
+//             where: {id: req.body.id},
+//             include: [db.category]
+//         })
+//         const foundClothing = await db.clothing.findOne({
+//             where: {id: req.body.id},
+//             include: [db.category]
+//         })
+//         const [foundOrCreated, isCreated] = await db.category.findOrCreate({
+//             where: {name: req.body.category}
+//         })
+//         await foundClothing.setCategory(foundOrCreated)
+//         res.redirect(`/clothing/${req.body.id}`)
+//     }
+//     catch(err){
+//         console.log(err)
+//         res.redirect(`/clothing/${req.body.id}`)
+//     }
+// })
 
 // DELETE /clothing/:id
 router.delete('/:id', isLoggedIn, (req,res) => {
@@ -141,24 +189,7 @@ router.delete('/:id', isLoggedIn, (req,res) => {
             })
             .catch(err => {
                 console.log(err)
-    })
-    .catch(err => {
-        console.log(err)
-    })
-    .catch(err => {
-        console.log(err)
-    })
-})
-    })
-})
-
-// POST /clothing/:id
-router.post('/:id', (req,res) => {
-    db.clothing.findByPk(req.body.clothingId)
-    .then(clothing => {
-        db.shelter.findByPk(req.body.shelterId)
-        .then(shelter => {
-            shelter.addClothing(clothing)
+            })
         })
         .catch(err => {
             console.log(err)
@@ -167,7 +198,36 @@ router.post('/:id', (req,res) => {
     .catch(err => {
         console.log(err)
     })
-    res.redirect(`/clothing/${req.body.clothingId}`)
+})
+
+// POST /clothing/:id
+router.post('/:id', isLoggedIn, (req,res) => {
+    db.user.findByPk(req.user.id)
+    .then(user => {
+        user.getClothings({where: {id: req.params.id}})
+        .then(clothing => {
+            db.clothing.findByPk(req.body.clothingId)
+            .then(clothing => {
+                db.shelter.findByPk(req.body.shelterId)
+                .then(shelter => {
+                    shelter.addClothing(clothing)
+                    res.redirect(`/clothing/${req.body.clothingId}`)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    })
+    .catch(err => {
+        console.log(err)
+    })
 })
 
 module.exports = router
